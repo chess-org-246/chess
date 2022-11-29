621fc797f853e9ac66a352428025530396575724
justinlu1 <j384lu@uwaterloo.ca>
2022-11-29 16:35:18 -0500

    set up .h and abstract block

---
+++ abstractblock.h
@@ -0,0 +1,18 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <vector>
+#include <memory> /* shared_ptr */
+
+class AbstractBlock {
+    std::vector<std::vector<shared_ptr<Cell>>> blockCells; /* 2d vector of cell ptrs, 4x4 */
+
+    public:
+        void left();
+        void right();
+        void rotate(bool ccw);
+        void down();
+        void drop() = 0;
+};
+
+#endif
