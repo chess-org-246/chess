04e7480c51433bd4be7cb4a7d666b5df1d3cad3d
justinlu1 <j384lu@uwaterloo.ca>
2022-11-29 15:53:50 -0500

    Added cell class

---
+++ cell.cc
@@ -0,0 +1,9 @@
+#include "cell.h"
+
+bool Cell::getHidden() {
+    return isHidden;
+}
+
+char Cell:getChar() {
+    return curChar;
+}
---
+++ cell.h
@@ -0,0 +1,12 @@
+#ifndef CELL_H
+#define CELL_H
+class Cell {
+    bool isHidden;
+    char curChar;
+
+    public:
+        bool getHidden();
+        char getChar();
+};
+
+#endif
